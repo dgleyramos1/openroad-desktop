4f9541e7d3296f380225517421bca4d7d6eb6d6c
Dgley Ramos <dgleysilva@gmail.com>
2023-01-28 09:36:28 -0300

    Iniciando projeto Open Road

---
+++ src/main/java/com/openroad/OpenRoadApplication.java
@@ -0,0 +1,13 @@
+package com.openroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openroad/OpenRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
