0d34ebb2b83f8ff9af1b64dd804a7feef15d24e5
Dgley Ramos <dgleysilva@gmail.com>
2023-01-28 09:44:24 -0300

    Model Admin Created

---
+++ src/main/java/com/openroad/api/model/Admin.java
@@ -0,0 +1,67 @@
+package com.openroad.api.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+}
