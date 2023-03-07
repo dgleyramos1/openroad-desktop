8dfa478bee8fa24a6a3f789db446555213adfd34
Dgley ramos <dgleysilva@gmail.com>
2023-03-07 16:04:51 -0300

    Criando controller para a tela signup

---
+++ src/main/java/com/openroad/controller/Signup.java
@@ -0,0 +1,52 @@
+package com.openroad.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+
+import com.openroad.ApplicationFX;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import net.rgielen.fxweaver.core.FxmlView;
+
+@Controller
+@FxmlView("signup.fxml")
+public class Signup {
+
+    @FXML
+    private TextField inputName;
+
+    @FXML
+    private PasswordField inputPassword;
+
+    @FXML
+    private TextField inputUsername;
+
+    private static Stage s;
+
+    @FXML
+    void handleSignup(MouseEvent event) {
+
+    }
+
+    public void signup(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Signup.class.getResource("signup.fxml"));
+        loader.setControllerFactory(ApplicationFX.getContextSpring()::getBean);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Open Road - Login");
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.show();
+        s = stage;
+    }
+}
